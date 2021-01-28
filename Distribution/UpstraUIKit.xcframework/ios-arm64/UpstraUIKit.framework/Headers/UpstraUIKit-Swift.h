b9865ce3c964eb979a6622aaa9c97caaccc22b39
Nontapat Siengsanor <siengnontapat@gmail.com>
2021-01-28 22:14:38 +0700

    Update Upstra 1.8.4

--- Distribution/UpstraUIKit.xcframework/ios-arm64/UpstraUIKit.framework/Headers/UpstraUIKit-Swift.h
+++ Distribution/UpstraUIKit.xcframework/ios-arm64/UpstraUIKit.framework/Headers/UpstraUIKit-Swift.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0fef921881fa20eb1efcbbc7ed7c88cb276cdef6aa422fc6d9ae7b6d2bad123
-size 60051
+oid sha256:9284575634d0e99b907e88b6c3e58b69b43faacc92e6ebe6bc971a5708c8c5e7
+size 60302
