7be41d8241a3ac29e5cc8ed465344ac7b95279ac
Nontapat Siengsanor <siengnontapat@gmail.com>
2021-06-01 10:34:38 +0700

    Update Upstra 2.0.1

--- Distribution/AmitySDK.xcframework/ios-x86_64-simulator/AmitySDK.framework/Headers/AmitySDK-Swift.h
+++ Distribution/AmitySDK.xcframework/ios-x86_64-simulator/AmitySDK.framework/Headers/AmitySDK-Swift.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2633b7704e6ab82fa0e38792c082bcb1f7a8c5477abd7cab580f4e0b34bf1c4d
-size 51919
+oid sha256:f61cb360b39c608b9f454fb946f82d00330fffaeae7dc87ed4d028b4d1bf1d07
+size 51820
